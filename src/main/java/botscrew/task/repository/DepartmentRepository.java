82f3993daf46e3f97dfc69e56812e40943ca6a1a
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-11-08 14:54:29 +0200

    Added annotations to repositories

--- src/main/java/botscrew/task/repository/DepartmentRepository.java
+++ src/main/java/botscrew/task/repository/DepartmentRepository.java
@@ -2,7 +2,9 @@ package botscrew.task.repository;
 
 import botscrew.task.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     boolean existsByName(String departmentName);
 }
--- src/main/java/botscrew/task/repository/EmployeeRepository.java
+++ src/main/java/botscrew/task/repository/EmployeeRepository.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query("FROM Employee e JOIN e.departments d "
             + "WHERE d.name = :departmentName AND e.id = d.head.id")
